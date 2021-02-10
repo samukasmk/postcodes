cc719e81eeb5537c579c8468f2fa2ad37bd0427c
Samuel Sampaio <samuel.sampaio@skyone.solutions>
2021-02-09 22:07:43 -0300

    [postcodes-uk] Create tests first to valid formats of postcodes uk

--- tests/test_postcodes_uk.py
+++ tests/test_postcodes_uk.py
@@ -2,13 +2,23 @@ import unittest
 from postcodes import PostCodeUK
 from parameterized import parameterized
 
+
 class TestPostCodeUK(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    @parameterized.expand(['AA9A 9AA', 'A9A 9AA', 'A9 9AA', 'A99 9AA', 'AA9 9AA', 'AA99 9AA'])
-    def test_postcode_uk_init(self, postcode_format):
-        postcode = PostCodeUK(postcode_format)
-        assert postcode != None
-
+    @parameterized.expand([['AA9A 9AA', 'AA', '9A', '9', 'AA'],
+                           ['A9A 9AA', 'A', '9A', '9', 'AA'],
+                           ['A9 9AA', 'A', '9', '9', 'AA'],
+                           ['A99 9AA', 'A', '99', '9', 'AA'],
+                           ['AA9 9AA', 'AA', '9', '9', 'AA'],
+                           ['AA99 9AA', 'AA', '99', '9', 'AA']])
+    def test_postcodes_uk_valid_formats(self, postcode, area, district, sector, unit):
+        postcode = PostCodeUK(postcode.lower())
+        self.assertEqual(postcode.is_valid(), True)
+        self.assertEqual(postcode.area, area)
+        self.assertEqual(postcode.district, district)
+        self.assertEqual(postcode.sector, sector)
+        self.assertEqual(postcode.unit, unit)
+        self.assertEqual(postcode.postcode, postcode)
