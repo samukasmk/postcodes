7c09e29ac489866a19bd5f090dbf9a1069dee4a7
Samuel Sampaio <samuel.sampaio@skyone.solutions>
2021-02-10 01:32:59 -0300

    [postcodes-uk] Check valid post codes

--- postcodes/postcodes_uk.py
+++ postcodes/postcodes_uk.py
@@ -1,15 +1,52 @@
+import re
+
+search_patterns = {'area': re.compile(r'^([A-Z]*).*'),
+                   'district': re.compile(r'^[A-Z]*([0-9][A-Z0-9]*)'),
+                   'sector': re.compile(r'.*[0-9].*([0-9])'),
+                   'unit': re.compile(r'.*[0-9]([A-Z]*)$')}
+
+validation_patterns = {'area': re.compile(r'^[A-Z]{1,2}$'),
+                       'district': re.compile(r'^[0-9][A-Z0-9]?$'),
+                       'sector': re.compile(r'^[0-9]$'),
+                       'unit': re.compile(r'^[A-Z]{2}$')}
+
+
 class PostCodeUK():
     """Object to parse postal code to UK format."""
 
     def __init__(self, postcode):
         self.__raw_postcode = postcode
         self.__full_postcode = self.__raw_postcode.upper()
-        self.__outward = None
-        self.__inward = None
-        self.__area = None
-        self.__district = None
-        self.__sector = None
-        self.__unit = None
+        self.__attribute_values = {}
+        self.__errors = {}
+
+        for attribute in ['area', 'district', 'sector', 'unit']:
+            # search attribute fragment in the full string
+            attribute_value = self.get_first_regex_group(regex_pattern=search_patterns[attribute],
+                                                         search_in_text=self.full_postcode)
+            self.__attribute_values[attribute] = attribute_value
+
+            # validate attribute fragment
+            attribute_is_valid = False
+            if attribute_value:
+                attribute_is_valid = self.regex_is_matched(regex_pattern=validation_patterns[attribute],
+                                                           search_in_text=attribute_value)
+            if not attribute_value or attribute_is_valid is False:
+                self.__errors[attribute] = f'Invalid {attribute} format.'
+
+    def get_first_regex_group(self, regex_pattern, search_in_text):
+        matched_search = regex_pattern.match(search_in_text)
+        if matched_search and matched_search.groups():
+            attribute_value = matched_search.groups()[0]
+            if attribute_value:
+                return attribute_value
+        return None
+
+    def regex_is_matched(self, regex_pattern, search_in_text):
+        matched_search = regex_pattern.match(search_in_text)
+        if matched_search and matched_search.group():
+            return True
+        return False
 
     @property
     def raw_postcode(self):
@@ -22,35 +59,31 @@ class PostCodeUK():
         return self.__full_postcode
 
     @property
-    def outward(self):
-        """Outward code attribute"""
-        return self.__outward
-
-    @property
-    def inward(self):
-        """Inward code attribute"""
-        return self.__inward
-
-    @property
     def area(self):
         """Area attribute"""
-        return self.__area
+        return self.__attribute_values['area']
 
     @property
     def district(self):
         """District attribute"""
-        return self.__district
+        return self.__attribute_values['district']
 
     @property
     def sector(self):
         """Sector attribute"""
-        return self.__sector
+        return self.__attribute_values['sector']
 
     @property
     def unit(self):
         """Unit attribute"""
-        return self.__unit
+        return self.__attribute_values['unit']
 
+    @property
+    def errors(self):
+        """Inward code attribute"""
+        return self.__errors
+
+    @property
     def is_valid(self):
         """Valid status attribute"""
-        pass
+        return not self.__errors
--- tests/test_postcodes_uk.py
+++ tests/test_postcodes_uk.py
@@ -24,12 +24,7 @@ class TestPostCodeUK(unittest.TestCase):
         postcode = PostCodeUK(raw_postcode)
         self.assertEqual(postcode.raw_postcode, raw_postcode)
         self.assertEqual(postcode.full_postcode, raw_postcode)
-
-        outward, inward = raw_postcode.split(' ')
-        self.assertEqual(postcode.outward, outward)
-        self.assertEqual(postcode.inward, inward)
-
-        self.assertEqual(postcode.is_valid(), True)
+        self.assertEqual(postcode.is_valid, True)
         self.assertEqual(postcode.area, area)
         self.assertEqual(postcode.district, district)
         self.assertEqual(postcode.sector, sector)
@@ -40,12 +35,7 @@ class TestPostCodeUK(unittest.TestCase):
         postcode = PostCodeUK(raw_postcode)
         self.assertEqual(postcode.raw_postcode, raw_postcode)
         self.assertEqual(postcode.full_postcode, raw_postcode)
-
-        outward, inward = raw_postcode.split(' ')
-        self.assertEqual(postcode.outward, outward)
-        self.assertEqual(postcode.inward, inward)
-
-        self.assertEqual(postcode.is_valid(), False)
+        self.assertEqual(postcode.is_valid, False)
         self.assertEqual(postcode.errors, {'area': 'Invalid area format.'})
 
     @parameterized.expand(['A 9A', 'AA 9A', 'AAA 9A', 'A 9AA', 'AA 9AA', 'AAA 9AA'])
@@ -53,12 +43,7 @@ class TestPostCodeUK(unittest.TestCase):
         postcode = PostCodeUK(raw_postcode)
         self.assertEqual(postcode.raw_postcode, raw_postcode)
         self.assertEqual(postcode.full_postcode, raw_postcode)
-
-        outward, inward = raw_postcode.split(' ')
-        self.assertEqual(postcode.outward, outward)
-        self.assertEqual(postcode.inward, inward)
-
-        self.assertEqual(postcode.is_valid(), False)
+        self.assertEqual(postcode.is_valid, False)
         self.assertEqual(postcode.errors, {'district': 'Invalid district format.'})
 
     @parameterized.expand(['AA9A AA', 'A9A AA', 'A9 AA', 'A99 AA', 'AA9 AA', 'AA99 AA'])
@@ -66,12 +51,7 @@ class TestPostCodeUK(unittest.TestCase):
         postcode = PostCodeUK(raw_postcode)
         self.assertEqual(postcode.raw_postcode, raw_postcode)
         self.assertEqual(postcode.full_postcode, raw_postcode)
-
-        outward, inward = raw_postcode.split(' ')
-        self.assertEqual(postcode.outward, outward)
-        self.assertEqual(postcode.inward, inward)
-
-        self.assertEqual(postcode.is_valid(), False)
+        self.assertEqual(postcode.is_valid, False)
         self.assertEqual(postcode.errors, {'sector': 'Invalid sector format.'})
 
     @parameterized.expand(['AA9A 9', 'A9A 9', 'A9 9', 'A99 9', 'AA9 9', 'AA99 9'])
@@ -79,10 +59,5 @@ class TestPostCodeUK(unittest.TestCase):
         postcode = PostCodeUK(raw_postcode)
         self.assertEqual(postcode.raw_postcode, raw_postcode)
         self.assertEqual(postcode.full_postcode, raw_postcode)
-
-        outward, inward = raw_postcode.split(' ')
-        self.assertEqual(postcode.outward, outward)
-        self.assertEqual(postcode.inward, inward)
-
-        self.assertEqual(postcode.is_valid(), False)
+        self.assertEqual(postcode.is_valid, False)
         self.assertEqual(postcode.errors, {'unit': 'Invalid unit format.'})
