e64a99fa6a5ecbe1d2d29d9772b999cbed2d5a71
Samuel Sampaio <samuel.sampaio@skyone.solutions>
2021-02-12 01:22:26 -0300

    [validations-improments] Improving validation methods

--- postcodes/uk.py
+++ postcodes/uk.py
@@ -1,12 +1,12 @@
 import re
+from typing import Tuple, Optional
 
 REGEX_AREA_VALIDATION = re.compile(r'^[A-Z]{1,2}$')
 REGEX_DISTRICT_VALIDATION = re.compile(r'^[0-9][A-Z0-9]?$')
 REGEX_SECTOR_VALIDATION = re.compile(r'^[0-9]$')
 REGEX_UNIT_VALIDATION = re.compile(r'^[A-Z]{2}$')
-REGEX_INSERT_SPACE_BEFORE_DIGITS = re.compile(r'([0-9]+)')
-REGEX_INSERT_SPACE_AFTER_DIGITS = re.compile(r'([0-9]+)')
-REGEX_SPLIT_SIDES_BY_SPACES = re.compile(r' +')
+REGEX_DIGITS = re.compile(r'([0-9]+)')
+REGEX_SPACES = re.compile(r' +')
 
 POSTCODE_VALIDATIONS = {'area': REGEX_AREA_VALIDATION,
                         'district': REGEX_DISTRICT_VALIDATION,
@@ -17,63 +17,24 @@ POSTCODE_VALIDATIONS = {'area': REGEX_AREA_VALIDATION,
 class PostCodeUK:
     """Object to parse postal code to UK format."""
 
-    def __init__(self, postcode):
+    def __init__(self, postcode: str):
         # define internal attributes
         self.__raw_postcode = postcode
         self.__full_postcode = postcode.upper()
+        self.__errors = None
         self.__outward = None
         self.__inward = None
-        self.__attributes = {'area': None, 'district': None, 'sector': None, 'unit': None}
-        self.__errors = {}
+        self.__area = None
+        self.__district = None
+        self.__sector = None
+        self.__unit = None
+        self.__attributes = ('area', 'district', 'sector', 'unit')
 
-        # splits outward and inward sides
-        self.__outward, self.__inward = self.split_sides_by_spaces(self.full_postcode)
-
-        # splits area and district sides
-        if self.outward:
-            outward_to_split = self.insert_space_before_digits(self.outward)
-            self.outward_splited = self.split_sides_by_spaces(outward_to_split)
-            self.__attributes['area'], self.__attributes['district'] = self.outward_splited
-
-        # splits sector and district unit
-        if self.inward:
-            if self.inward[0].isdigit():
-                inward_to_split = self.insert_space_after_digits(self.inward)
-            else:
-                inward_to_split = ' ' + self.inward
-            self.inward_splited = self.split_sides_by_spaces(inward_to_split)
-            self.__attributes['sector'], self.__attributes['unit'] = self.inward_splited
-
-        # parses postcode format in separated pieces
-        for name in list(self.__attributes.keys()):
-            regex_pattern = POSTCODE_VALIDATIONS[name]
-            attribute_value = self.attributes.get(name)
-            if not attribute_value or not regex_pattern.match(attribute_value):
-                self.__errors[name] = f'Invalid {name} format.'
-
-    def insert_space_before_digits(self, text_to_insert):
-        return re.sub(r'([0-9]+)', r' \1', text_to_insert)
-
-    def insert_space_after_digits(self, text_to_insert):
-        return re.sub(r'([0-9]+)', r'\1 ', text_to_insert)
-
-    def insert_space_at_beginning(self, text_to_insert):
-        return ' ' + text_to_insert
-
-    def split_sides_by_spaces(self, text_to_split):
-        left_side = None
-        right_side = None
-        splited_sides = re.split(r' +', text_to_split)
-        if splited_sides:
-            left_side = splited_sides[0]
-            if len(splited_sides) > 1:
-                right_side = ''.join(splited_sides[1:])
-        return left_side, right_side
-
-    @property
-    def regex_patterns(self):
-        """Regexp patterns"""
-        return self.__regex_patterns
+        # parses postcode format
+        self.__outward, self.__inward = self.__get_outward_and_inward()
+        self.__area, self.__district = self.__get_area_and_district()
+        self.__sector, self.__unit = self.__get_sector_and_unit()
+        self.__errors = self.__validate_postcode_attributes_format()
 
     @property
     def raw_postcode(self):
@@ -86,13 +47,18 @@ class PostCodeUK:
         return self.__full_postcode
 
     @property
+    def errors(self):
+        """Errors dict with exact postcode attribute that is incorrectly formatted"""
+        return self.__errors
+
+    @property
     def outward(self):
-        """Outward attribute"""
+        """Outward side"""
         return self.__outward
 
     @property
     def inward(self):
-        """Inward attribute"""
+        """Inward side"""
         return self.__inward
 
     @property
@@ -102,30 +68,89 @@ class PostCodeUK:
 
     @property
     def area(self):
-        """Area attribute"""
-        return self.__attributes['area']
+        """Area postcode attribute"""
+        return self.__area
 
     @property
     def district(self):
-        """District attribute"""
-        return self.__attributes['district']
+        """District postcode attribute"""
+        return self.__district
 
     @property
     def sector(self):
-        """Sector attribute"""
-        return self.__attributes['sector']
+        """Sector postcode attribute"""
+        return self.__sector
 
     @property
     def unit(self):
-        """Unit attribute"""
-        return self.__attributes['unit']
-
-    @property
-    def errors(self):
-        """Errors dict"""
-        return self.__errors
+        """Unit postcode attribute"""
+        return self.__unit
 
     @property
     def is_valid(self):
         """Validation status"""
         return not self.__errors
+
+    def __get_outward_and_inward(self):
+        """Splits full postcode string in outward and inward sides"""
+        return self.__split_sides_by_spaces(self.full_postcode)
+
+    def __get_area_and_district(self):
+        """Splits outward string in area and district sides"""
+        if not self.outward:
+            return None, None
+
+        outward_to_split = self.__insert_space_before_digits(self.outward)
+        return self.__split_sides_by_spaces(outward_to_split)
+
+    def __get_sector_and_unit(self):
+        """Splits inward string in sector and unit sides"""
+        if not self.inward:
+            return None, None
+
+        if self.inward[0].isdigit():
+            inward_to_split = self.__insert_space_after_digits(self.inward)
+        else:
+            inward_to_split = self.__insert_space_at_beginning(self.inward)
+
+        return self.__split_sides_by_spaces(inward_to_split)
+
+    def __validate_postcode_attributes_format(self):
+        """Parses postcode format in separated pieces
+           The purpose of validating separate pieces is to obtain depth of
+           understanding on the exact attribute of the p that is incorrect"""
+        errors = {}
+        for name in self.attributes:
+            regex_pattern = POSTCODE_VALIDATIONS[name]
+            value = getattr(self, name)
+            if not value or not regex_pattern.match(value):
+                errors[name] = f'Invalid {name} format.'
+        return errors
+
+    @staticmethod
+    def __insert_space_before_digits(text_to_insert: str) -> str:
+        """Formats string to split inserting spaces before digits"""
+        return REGEX_DIGITS.sub(r' \1', text_to_insert)
+
+    @staticmethod
+    def __insert_space_after_digits(text_to_insert: str) -> str:
+        """Formats string to split inserting spaces after digits"""
+        return REGEX_DIGITS.sub(r'\1 ', text_to_insert)
+
+    @staticmethod
+    def __insert_space_at_beginning(text_to_insert: str) -> str:
+        """Formats string to split inserting spaces at the string begging"""
+        return ' ' + text_to_insert
+
+    @staticmethod
+    def __split_sides_by_spaces(text_to_split: str) -> Tuple[Optional[str], Optional[str]]:
+        """Splits string into two pieces
+           First piece is the first element found and second piece is the rest of matches"""
+        left_side = None
+        right_side = None
+        splited_sides = REGEX_SPACES.split(text_to_split)
+        if splited_sides:
+            left_side = splited_sides[0]
+            if len(splited_sides) > 1:
+                right_side = ''.join(splited_sides[1:])
+        return left_side, right_side
